fe929281348543f70c38a5c7068ed04853e09aa3
Camilo Eduardo Castro Torres <c.castrot@uniandes.edu.co>
2025-05-27 10:14:07 -0500

    k

--- DataStructures/List/list_node.py
+++ DataStructures/List/list_node.py
@@ -1,17 +1,4 @@
-"""
-  Estructura que contiene la información a guardar en una lista encadenada
-"""
-
-
 def new_single_node(element):
-    """ Estructura que contiene la información a guardar en una lista encadenada
-
-        :param element: Elemento a guardar en el nodo
-        :type element: any
-
-        :returns: Nodo creado
-        :rtype: dict
-    """
     node = {'info': element, 'next': None}
     return (node)
 
@@ -21,8 +8,6 @@ def get_element(node):
 
 
 def new_double_node(element):
-    """ Estructura que contiene la información a guardar en un nodo de una lista doblemente encadenada
-    """
     node = {'info': element,
             'next': None,
             'prev': None
